43c666e7180feac25f0b979974819fd8af1a91da
Carsten Lamm <CarstenLamm@googlemail.com>
2014-07-23 00:21:18 +0200

    adds basic html upload

--- server/server.js
+++ server/server.js
@@ -1,6 +1,10 @@
 'use strict';
 
-var express = require('express');
+var express = require('express'),
+    Busboy = require('busboy'),
+    path = require('path'),
+    fs = require('fs'),
+    util = require('util');
 
 var app = express();
 
@@ -11,6 +15,31 @@ app.configure(function () {
 
 app.get('/', function (req, res) {
     res.send('works');
+});
+
+
+app.post('/upload_file_multipart', function (req, res) {
+
+    var busboy = new Busboy({ headers: req.headers });
+
+    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
+        console.log('receiving ...', fieldname, filename);
+
+        var saveTo = path.join(__dirname, '/public/upload', path.basename(filename));
+
+        var fstream = fs.createWriteStream(saveTo);
+        file.pipe(fstream);
+
+        fstream.on('close', function () {
+            var path = fstream.path.split('/server/public/')[1];
+
+            res.writeHead(200, { 'Connection': 'close' });
+            var link = util.format('<a href="%s">Works. Your file: %s<a/>', path, filename)
+            res.end(link);
+        })
+    });
+
+    return req.pipe(busboy);
 });
 
 var server = app.listen(3000, function () {
